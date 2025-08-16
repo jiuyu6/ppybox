2ff34019efb4ec37a036af240fd4e4da9f35343e
jiuyu6 <102639690+jiuyu6@users.noreply.github.com>
2025-08-16 21:10:28 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,7 +1,7 @@
 const CUSTOMER_SITES = {
     qiqi: {
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七资源',
+        api: 'https://slapibf.com/api.php/provide/vod',
+        name: 'sese资源',
     }
 };
 
